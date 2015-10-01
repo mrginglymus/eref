2b75848d813df26153416c1a1195b1e5e7298b1f
Bill Collins <bilbocollins@gmail.com>
2015-10-01 08:51:46 +0100

    stuff

--- review/index.php
+++ review/index.php
@@ -3,6 +3,10 @@
 
 require_once('../base.php');
 
+if (isset($_GET['confirmdoc']) && isset($_GET['confirmporky'])) {
+  header('Location: ../thanks');
+}
+
 echo $twig->render('review.html', array(
   'subtitle'=>'Academic Reference for Miss Sarah Williams',
   'current'=>'review',
---
+++ thanks/index.php
@@ -0,0 +1,10 @@
+<?php 
+
+require_once('../base.php');
+
+echo $twig->render('thanks.html', array(
+  'subtitle'=>'Reference Submitted',
+  ));
+?>
+
+
