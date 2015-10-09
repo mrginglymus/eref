71fc37f7d919409eca1b23687d9cea53e24f10ed
Bill Collins <bilbocollins@gmail.com>
2015-10-09 13:37:31 +0100

    templates mergh

---
+++ scrollspy/index.php
@@ -0,0 +1,22 @@
+<?php 
+
+
+require_once('../base.php');
+
+if (isset($_GET['save'])) {
+  $save=true;
+}
+
+if (isset($_GET['missing_fields'])) {
+  $missing_fields=true;
+}
+
+echo $twig->render('scrollspy.html', array(
+  'subtitle'=>'Academic Reference for Miss Sarah Williams',
+  'current'=>'overview',
+  'save'=>$save,
+  'missing_fields'=>$missing_fields
+));
+?>
+
+
